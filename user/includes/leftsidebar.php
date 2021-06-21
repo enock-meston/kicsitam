2d02eedfd124245daabdc03cf22dea95bd480803
Enock <ndagijimanaenock11@gmail.com>
2021-06-21 18:06:21 +0200

    add user dashboard

---
+++ user/includes/leftsidebar.php
@@ -0,0 +1,42 @@
+            <div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <ul>
+                            <li class="menu-title">Navigation</li>
+
+                            <li class="has_sub">
+                                <a href="dashboard.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i>
+                                    <span> Dashboard </span> </a>
+
+                            </li>
+
+                            <!--tools management  -->
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i
+                                        class="mdi mdi-format-list-bulleted"></i> <span> Tools </span> <span
+                                        class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-group.php">Add Tools</a></li>
+                                    <li><a href="#">Manage Tools</a></li>
+                                    <li><a href="#">Trash Groups</a></li>
+                                </ul>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                    <div class="clearfix"></div>
+
+                    <div class="help-box">
+                        <h5 class="text-muted m-t-0">For Help ?</h5>
+                        <p style="font-size:10px;" class=""><span class="text-custom">Email:</span> <br />
+                            ndagijimanaenock11@gmail.com</p>
+                    </div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
