bb0883b84be2c46af84f3be43376706d74e0c0d2
Enock Ndagijimana <58587259+enock-meston@users.noreply.github.com>
2021-07-07 17:48:57 +0200

    home

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,26 +8,67 @@
     <title>welcome to kicsitam</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+
 
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="bootstrap/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="imagess/logo.png" type="image/x-icon"/>
+    <link rel="shortcut icon" href="imagess/logo.png" type="image/x-icon" />
 </head>
-<body>
-    <!-- Navbar content --> 
-<?php 
-    include 'include/menus.php';
-?>
 
-<div class="bg">
-    <div class="text">
-        <h1 style="font-size:50px">Welcome To KICSIT Asset Management <br><center>(KICSITAM)</center></h1>
-     </div>
-</div>
+<body>
+    <!-- Navbar content -->
+    <?php
+    include 'include/menus.php';
+    ?>
+
+    <div class="bg">
+        <div class="text">
+            <!-- <h1 style="font-size:50px">Welcome To KICSIT Asset Management <br><center>(KICSITAM)</center></h1> -->
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="imagess/code.jpeg" alt="First slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>sdhasja</h5>
+                            <p>.dbjodododf..</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="imagess/download1.jpg" alt="Second slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>sdhasja</h5>
+                            <p>.dbjodododf..</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="imagess/it-equipments-services-500x500.png" alt="Third slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>sdhasja</h5>
+                            <p>.dbjodododf..</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
 
 
 </body>
+
 </html>
