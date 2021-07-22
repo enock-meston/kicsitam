8a37c5bc7004deeb1a5ec55cec722db12413c239
enock-meston <ndagijimanaenock11@gmail.com>
2021-07-22 20:47:52 +0200

    add-durable-asset

---
+++ user/add-st-durable-asset.php
@@ -0,0 +1,313 @@
+<?php
+session_start();
+include("phpqrcode/qrlib.php");
+include('includes/config.php');
+error_reporting(0);
+if (strlen($_SESSION['id']) == 0) {
+    header('location:index.php');
+} else {
+
+    // For adding post  
+    if (isset($_POST['submit'])) {
+
+        $posttitle = addslashes($_POST['toolname']);
+        $studentid = $_POST['studentID'];
+        $serial=$_POST['serial'];
+        $MacAddress =$_POST['MacAddress'];
+        //qrpath
+        $qrpath='img/';
+        $file=$qrpath.$_POST['serial'].'.png';  // is path and name of qr code in the database
+        // qrname`
+        $qrkey=$serial." - ".$studentid;  // is serial_number in database
+        $returingdate = addslashes($_POST['returingdate']);
+        $imgfile = $_FILES["postimage"]["name"];
+        // get the image extension
+        $extension = substr($imgfile, strlen($imgfile) - 4, strlen($imgfile));
+        // allowed extensions
+        $allowed_extensions = array(".jpg", "jpeg", ".png", ".gif");
+        // Validation for allowed extensions .in_array() function searches an array for a specific value.
+        if (!in_array($extension, $allowed_extensions)) {
+            echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+        } else {
+            //rename the image file
+            $imgnewfile = md5($imgfile) . $extension;
+            // Code for move image into directory
+            move_uploaded_file($_FILES["postimage"]["tmp_name"], "toolimages/" . $imgnewfile);
+
+            $status = 1;
+            // $respone_status=0;
+            // qr method
+            Qrcode::png($qrkey,$file);
+            $query = mysqli_query($con, "INSERT INTO `durable-student-Asset`(`assetname`, 
+            `serialNumber`, `ToolImage`, `studentID`, `MAC Adress`, `QR-Code`, `QR-CodeImage`, 
+            `returningDate`, `Active_Status`) VALUES ('$posttitle','$serial','$imgnewfile','$studentid','$MacAddress','$qrkey','$file','$returingdate','$status')");
+            if ($query) {
+                $msg = "Asset successfully added ";
+                // echo "<script type='text/javascript'> document.location = 'add-tool.php'; </script>";
+            } else {
+                $error = "Something went wrong . Please try again.";
+            }
+        }
+
+        
+    }
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <!-- App title -->
+        <title> ADD Durable Aseet - <?php echo $_SESSION['fn'] . " " . $_SESSION['ln']; ?>|</title>
+
+        <!-- Summernote css -->
+        <link href="../plugins/summernote/summernote.css" rel="stylesheet" />
+
+        <!-- Select2 -->
+        <link href="../plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Jquery filer css -->
+        <link href="../plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
+        <link href="../plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
+
+        <!-- App css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+        <script src="assets/js/modernizr.min.js"></script>
+        <script>
+            function getSubCat(val) {
+                $.ajax({
+                    type: "POST",
+                    url: "get_subcategory.php",
+                    data: 'catid=' + val,
+                    success: function(data) {
+                        $("#subcategory").html(data);
+                    }
+                });
+            }
+        </script>
+    </head>
+
+
+    <body class="fixed-left">
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Top Bar Start -->
+            <?php include('includes/topheader.php'); ?>
+            <!-- ========== Left Sidebar Start ========== -->
+            <?php include('includes/leftsidebar.php'); ?>
+            <!-- Left Sidebar End -->
+
+
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="page-title-box">
+                                    <h4 class="page-title">Add Asset </h4>
+                                    <ol class="breadcrumb p-0 m-0">
+                                        <li>
+                                            <a href="#">Asset</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Add Asset </a>
+                                        </li>
+                                        <li class="active">
+                                            Add Asset
+                                        </li>
+                                    </ol>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end row -->
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <!---Success Message--->
+                                <?php if ($msg) { ?>
+                                    <div class="alert alert-success" role="alert">
+                                        <strong>Well done!</strong> <?php echo htmlentities($msg); ?>
+                                    </div>
+                                <?php } ?>
+
+                                <!---Error Message--->
+                                <?php if ($error) { ?>
+                                    <div class="alert alert-danger" role="alert">
+                                        <strong>Oh snap!</strong> <?php echo htmlentities($error); ?>
+                                    </div>
+                                <?php } ?>
+
+
+                            </div>
+                        </div>
+
+
+                        <div class="row">
+                            <div class="col-md-10 col-md-offset-1">
+                                <div class="p-6">
+                                    <div class="">
+                                        <form name="addpost" method="post" enctype="multipart/form-data">
+                                            <div class="form-group m-b-20">
+                                                <label for="exampleInputEmail1">Asset Name </label>
+                                                <input type="text" class="form-control" id="posttitle" name="toolname" placeholder="Enter Asset Name" required>
+                                            </div>
+
+                                            <div class="form-group m-b-20">
+                                                <label for="exampleInputEmail1">Serial Number</label>
+                                                <input type="text" class="form-control" id="posttitle" name="serial" placeholder="Enter Serial Number" required>
+                                            </div>
+
+                                            <div class="form-group m-b-20">
+                                                <label for="exampleInputEmail1">Mac Address</label>
+                                                <input type="text" class="form-control" id="posttitle" name="MacAddress" placeholder="Enter Mac Address" required>
+                                            </div>
+
+                                            
+
+                                            <div class="form-group m-b-20">
+                                                <label for="exampleInputEmail1">Select Student</label>
+                                                <select class="form-control" name="studentID" id="category" onChange="getSubCat(this.value);" required>
+                                                    <option value="">Select Student </option>
+                                                    <?php
+                                                    // Feching active categories
+                                                    $ret = mysqli_query($con, "SELECT * from  tblstudent where Active_Status=1");
+                                                    while ($result = mysqli_fetch_array($ret)) {
+                                                    ?>
+                                                        <option value="<?php echo htmlentities($result['id']); ?>"><?php echo htmlentities($result['Firstname'])." ". htmlentities($result['Lastname']); ?></option>
+                                                    <?php } ?>
+
+                                                </select>
+                                            </div>
+
+                                            <div class="form-group m-b-20">
+                                                <label for="exampleInputEmail1">Returning Date</label>
+                                                <input type="Date" class="form-control" id="posttitle" name="returingdate">
+                                            </div>
+
+                                            
+
+
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <div class="card-box">
+                                                        <h4 class="m-b-30 m-t-0 header-title"><b>Asset Image</b></h4>
+                                                        <input type="file" class="form-control" id="postimage" name="postimage" required>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                            <button type="submit" name="submit" class="btn btn-success waves-effect waves-light">Save</button>
+                                            <button type="button" class="btn btn-danger waves-effect waves-light">Discard</button>
+                                        </form>
+                                    </div>
+                                </div> <!-- end p-20 -->
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+ 
+
+                    </div> <!-- container -->
+
+                </div> <!-- content -->
+
+                <?php include('includes/footer.php'); ?>
+
+            </div>
+
+
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+        <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!--Summernote js-->
+        <script src="../plugins/summernote/summernote.min.js"></script>
+        <!-- Select 2 -->
+        <script src="../plugins/select2/js/select2.min.js"></script>
+        <!-- Jquery filer js -->
+        <script src="../plugins/jquery.filer/js/jquery.filer.min.js"></script>
+
+        <!-- page specific js -->
+        <script src="assets/pages/jquery.blog-add.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+        <script>
+            jQuery(document).ready(function() {
+
+                $('.summernote').summernote({
+                    height: 240, // set editor height
+                    minHeight: null, // set minimum height of editor
+                    maxHeight: null, // set maximum height of editor
+                    focus: false // set focus to editable area after initializing summernote
+                });
+                // Select2
+                $(".select2").select2();
+
+                $(".select2-limiting").select2({
+                    maximumSelectionLength: 2
+                });
+            });
+        </script>
+        <script src="../plugins/switchery/switchery.min.js"></script>
+
+        <!--Summernote js-->
+        <script src="../plugins/summernote/summernote.min.js"></script>
+
+
+
+
+    </body>
+
+    </html>
+
+<?php
+}
+?>
